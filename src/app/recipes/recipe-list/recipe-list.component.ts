b6da45e07c62b5ae6623801f2c5d4a0b7a8e4620
Soumya Gade <soumyargade@users.noreply.github.com>
2020-06-29 20:43:30 -0400

    Added some recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe("SMOOTHIE BOWL", "Nutritious AND aesthetic", "https://minimalistbaker.com/wp-content/uploads/2016/07/The-PERFECT-5-minute-Smoothie-Bowl-Simple-ingredients-naturally-sweet-SO-healthy-vegan-glutenfree-smoothiebowl-recipe.jpg"), 
+    new Recipe("CHICKEN & SWEET POTATO", "Combo of sweet and spicy", "https://pinchofyum.com/wp-content/uploads/Chicken-Sweet-Potato-Meal-Prep-Bowls-7.jpg"), 
+    new Recipe("AVOCADO TOAST", "Excellent source of fiber", "https://www.twopeasandtheirpod.com/wp-content/uploads/2017/06/Everything-Bagel-Avocado-Toast-2.jpg")
 
-  ngOnInit(): void {
-  }
+  ];
+
+  constructor() { }
+  ngOnInit(): void { }
 
 }
