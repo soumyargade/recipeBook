a2fb61f7bb612c380f2968a0a417e3ed3ae1f1bf
Soumya Gade <soumyargade@users.noreply.github.com>
2020-07-06 18:37:35 -0400

    Added routing functionality for main pages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,9 +6,9 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 
 export class HeaderComponent {
-    @Output() featureSelected = new EventEmitter<string>();
+    // @Output() featureSelected = new EventEmitter<string>();
 
-    onSelect(selection: string) {
-        this.featureSelected.emit(selection);
-    }
+    // onSelect(selection: string) {
+        // this.featureSelected.emit(selection);
+    // }
 }
