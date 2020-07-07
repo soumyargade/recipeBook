59c818717ee35b1b3fac838d6aecd2d39af21d6f
Soumya Gade <soumyargade@users.noreply.github.com>
2020-07-06 21:15:19 -0400

    Routing for child components

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,10 +10,18 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
-  ngOnInit(): void { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  ngOnInit() {
+    // const id = this.route.snapshot.params['id'];
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id)}
+    );
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToList(this.recipe.ingredients);
