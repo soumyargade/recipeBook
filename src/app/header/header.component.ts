769709aba5c2ddcf110dcdb592b204e07ea7be4c
Soumya Gade <soumyargade@users.noreply.github.com>
2020-07-13 15:41:09 -0400

    Sending and fetching HTTP requests

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+
+    constructor(private dataStorageService: DataStorageService) { }
+
+    onSaveData() {
+        this.dataStorageService.saveRecipes();
+    }
+
+    onFetchData() {
+        this.dataStorageService.fetchRecipes().subscribe();
+    }
+    
     // @Output() featureSelected = new EventEmitter<string>();
 
     // onSelect(selection: string) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -13,34 +13,42 @@ export class RecipeService {
     // recipeSelected = new Subject<Recipe>();
 
     // Can't be directly accessed from the outside
-    private recipes: Recipe[] = [
-        new Recipe(
-            "SMOOTHIE BOWL", 
-            "Nutritious AND aesthetic", 
-            "https://minimalistbaker.com/wp-content/uploads/2016/07/The-PERFECT-5-minute-Smoothie-Bowl-Simple-ingredients-naturally-sweet-SO-healthy-vegan-glutenfree-smoothiebowl-recipe.jpg", 
-            [
-                new Ingredient('Fresh Berries', 30), 
-                new Ingredient('Milk', 2)
-            ]), 
-        new Recipe(
-            "CHICKEN & SWEET POTATO", 
-            "Combo of sweet and spicy", 
-            "https://pinchofyum.com/wp-content/uploads/Chicken-Sweet-Potato-Meal-Prep-Bowls-7.jpg", 
-            [
-                new Ingredient('Chicken', 5), 
-                new Ingredient('Sweet Potato', 6)
-            ]), 
-        new Recipe(
-            "AVOCADO TOAST", 
-            "Excellent source of fiber", 
-            "https://www.twopeasandtheirpod.com/wp-content/uploads/2017/06/Everything-Bagel-Avocado-Toast-2.jpg", 
-            [
-                new Ingredient('Avocado', 2), 
-                new Ingredient('Bread', 3)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+        // new Recipe(
+            // "SMOOTHIE BOWL", 
+            // "Nutritious AND aesthetic", 
+            // "https://minimalistbaker.com/wp-content/uploads/2016/07/The-PERFECT-5-minute-Smoothie-Bowl-Simple-ingredients-naturally-sweet-SO-healthy-vegan-glutenfree-smoothiebowl-recipe.jpg", 
+            // [
+                // new Ingredient('Fresh Berries', 30), 
+                // new Ingredient('Milk', 2)
+            // ]), 
+        // new Recipe(
+            // "CHICKEN & SWEET POTATO", 
+            // "Combo of sweet and spicy", 
+            // "https://pinchofyum.com/wp-content/uploads/Chicken-Sweet-Potato-Meal-Prep-Bowls-7.jpg", 
+            // [
+                // new Ingredient('Chicken', 5), 
+                // new Ingredient('Sweet Potato', 6)
+            // ]), 
+        // new Recipe(
+            // "AVOCADO TOAST", 
+            // "Excellent source of fiber", 
+            // "https://www.twopeasandtheirpod.com/wp-content/uploads/2017/06/Everything-Bagel-Avocado-Toast-2.jpg", 
+            // [
+                // new Ingredient('Avocado', 2), 
+                // new Ingredient('Bread', 3)
+            // ])
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private shoppingListService: ShoppingListService) { }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        // Informing interested places that there are new recipes
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         // Returns a copy of the recipes array
